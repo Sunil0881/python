6022889de8d2be987a12801e51302634b1921593
Sunil0881 <112623005+Sunil0881@users.noreply.github.com>
2023-03-14 21:39:42 +0530

    Add files via upload

---
+++ web scraper.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.example.com"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find all the links on the page
+links = []
+for link in soup.find_all("a"):
+    links.append(link.get("href"))
+
+# Find all the images on the page
+images = []
+for img in soup.find_all("img"):
+    images.append(img.get("src"))
+
+# Find the page title
+title = soup.title.string
+
+# Print the results
+print("Links:")
+for link in links:
+    print(link)
+
+print("\nImages:")
+for image in images:
+    print(image)
+
+print("\nTitle:")
+print(title)
