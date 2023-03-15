10ed6f855a98392363cd57cffc3d75f7c3e057db
Sunil0881 <112623005+Sunil0881@users.noreply.github.com>
2023-03-15 21:41:47 +0530

    Add files via upload

---
+++ password gendrator.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+# Define the length of the password
+length = 12
+
+# Define the characters to use in the password
+characters = string.ascii_letters + string.digits + string.punctuation
+
+# Generate a random password
+password = ''.join(random.choice(characters) for i in range(length))
+
+# Print the password
+print("Your random password is:", password)
