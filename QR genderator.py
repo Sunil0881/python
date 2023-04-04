e03aa141ad7eb1830e6974679fb0aa436a381bc7
Sunil0881 <112623005+Sunil0881@users.noreply.github.com>
2023-04-04 09:34:25 +0530

    Add files via upload

---
+++ QR genderator.py
@@ -0,0 +1,21 @@
+import qrcode
+
+# Create QR code instance
+qr = qrcode.QRCode(
+    version=1,  # QR code version
+    box_size=10,  # Size of each box in the QR code
+    border=5  # Number of boxes to use for the border
+)
+
+# Data to encode in the QR code
+data = "https://www.example.com"
+
+# Add data to QR code
+qr.add_data(data)
+
+# Generate QR code as an image
+qr.make(fit=True)
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image to a file
+img.save("example_qr_code.png")
