72e6cfdf8793ead4d5c90a8ec21c8f26e0da9493
Sunil0881 <112623005+Sunil0881@users.noreply.github.com>
2023-04-11 23:19:14 +0530

    Update password gendrator.py

--- password gendrator.py
+++ password gendrator.py
@@ -1,14 +1,14 @@
 import random
 import string
 
-# Define the length of the password
+# Define the length of the password.
 length = 15
 
-# Define the characters to use in the password
+# Define the characters to use in the password.
 characters = string.ascii_letters + string.digits + string.punctuation
 
-# Generate a random password
+# Generate a random password.
 password = ''.join(random.choice(characters) for i in range(length))
 
-# Print the password
+# Print the password.
 print("Your random password is:", password)
